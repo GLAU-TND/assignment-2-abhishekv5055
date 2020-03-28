7458a65e4c3d952c21fcba0904003af0f54dfdd9
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-03-28 10:42:22 +0530

    create getter setter.

--- src/problem3/student/Student.java
+++ src/problem3/student/Student.java
@@ -2,4 +2,12 @@ package problem3.student;
 
 public class Student{
     int rollNumber;
+
+    public int getRollNumber() {
+        return rollNumber;
+    }
+
+    public void setRollNumber(int rollNumber) {
+        this.rollNumber = rollNumber;
+    }
 }
