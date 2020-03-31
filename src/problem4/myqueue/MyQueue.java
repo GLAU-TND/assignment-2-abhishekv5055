2868a0c16697c71727b942ad2f4f85f2561459b9
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-03-31 20:23:39 +0530

    create getter and setter.

--- src/problem4/myqueue/MyQueue.java
+++ src/problem4/myqueue/MyQueue.java
@@ -11,5 +11,21 @@ public class MyQueue {
    private static class Node{
       int data;
       Node next;
+
+      public int getData() {
+         return data;
+      }
+
+      public void setData(int data) {
+         this.data = data;
+      }
+
+      public problem4.myqueue.MyQueue.Node getNext() {
+         return next;
+      }
+
+      public void setNext(problem4.myqueue.MyQueue.Node next) {
+         this.next = next;
+      }
    }
 }
